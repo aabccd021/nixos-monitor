6d04484a1af882e272fb58fbcb9c4282df551c0c
aabccd021 <aabccd021@gmail.com>
2025-06-18 12:26:31 +0700

    Remove .service suffixes from service names when generating services script.

--- generate-services.sh
+++ generate-services.sh
@@ -1,18 +1,11 @@
-filename=${1:-}
-
-if [ -z "$filename" ]; then
-  filename="$(hostname)-services.nix"
-fi
-
-# remove .service suffix from service names
 services=$(
   systemctl list-units --no-legend --no-pager --type service --all |
     sed 's/^●//' |
     awk '{print $1}' |
     sed 's/\.service$//'
 )
-echo "[" >"$filename"
+echo "["
 for service in $services; do
-  echo "  \"$service\"" >>"$filename"
+  echo "  \"$service\""
 done
-echo "]" >>"$filename"
+echo "]"
