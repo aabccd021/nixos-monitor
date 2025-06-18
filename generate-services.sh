5bcbc094ff4479fb00b25856a97d5145dd00bedb
aabccd021 <aabccd021@gmail.com>
2025-06-18 09:58:22 +0700

    Refactor: Removed .service suffix, added skip for notify-failure services

--- generate-services.sh
+++ generate-services.sh
@@ -4,13 +4,19 @@ if [ -z "$filename" ]; then
   filename="$(hostname)-services.nix"
 fi
 
+# remove .service suffix from service names
 services=$(
   systemctl list-units --no-legend --no-pager --type service --all |
     sed 's/^●//' |
-    awk '{print $1}'
+    awk '{print $1}' |
+    sed 's/\.service$//'
 )
 echo "[" >"$filename"
 for service in $services; do
+  echo "Processing service: $service"
+  if echo "$service" | grep -q '^notify-failure@'; then
+    continue
+  fi
   echo "  \"$service\"" >>"$filename"
 done
 echo "]" >>"$filename"
